fae85451dfb4b5d2b2042f718bb22ccd33e5e8ca
lakhwinder singh <44182274+lakhwindersingh738047@users.noreply.github.com>
2019-10-12 14:07:53 -0400

    divided index.html
    divided index.html file into three parts.

---
+++ top.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Navbar</title>
+
+	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
+
+<style type="text/css">
+	
+
+.signupbox{
+
+
+	padding-top: 15px;	
+	padding-bottom: 15px;
+}
+
+
+</style>
+</head>
+<body>
+
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+			 
+
+		<div class="container-fluid">
+
+
+			  <a class="navbar-brand" href="index.php">EMS</a>
+			  		
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+	
+			   		<span class="navbar-toggler-icon"></span>
+			 
+			</button>
+
+
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    
+			    <ul class="navbar-nav ml-auto">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+			      </li>
+
+			      <li class="nav-item">
+			        <a class="nav-link" href="aboutus.html">About Us</a>
+			      </li>
+					
+				  <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="		false">Services</a>
+			        
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="contactus.html">Contact Us</a>
+			          <a class="dropdown-item" href="pricing.html">Pricing</a>
+			        
+			          <div class="dropdown-divider"></div>
+			        	  <a class="dropdown-item" href="#">Sign Up</a>
+			        </div>
+			      </li>
+
+
+			    
+			    </ul>
+			</div>
+
+			<!--
+			    <form class="form-inline my-2 my-lg-0">
+			      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			    </form>
+
+
+
+			    			  <a class="nav-link" href="#" style=" visibility: hidden;">Log Out</a>
+
+			  -->
+
+
+
+		</nav>
+</div>
