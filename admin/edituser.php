17a4bd0081b73fc662c60310410d884583515b2a
c0734815 <44182257+c0734815@users.noreply.github.com>
2019-11-02 15:48:01 -0400

    updated user page in admin and elevated user module

---
+++ admin/edituser.php
@@ -0,0 +1,115 @@
+<?php include"top.php"; 
+  $userinfo=$_GET['email'];
+
+ 
+if (isset($_POST['update'])) {
+
+
+	$fname=$_POST['fname'];
+	$lname=$_POST['lname'];
+	$email=$_POST['email'];
+	$dob=$_POST['dob'];
+	$phone=$_POST['phone'];
+	$department=$_POST['department'];
+
+
+	
+
+		$sql="UPDATE `users` SET `fname`='$fname',`lastname`='$lname',`dob`='$dob',`email`='$email',`department`='$department',`phone`='$phone' WHERE `email`='$userinfo'";
+
+		echo $sql;
+
+		 if (mysqli_query($conn, $sql)) {
+               echo "New record created successfully";
+            } 
+            else {
+               echo "Error: " . $sql . "" . mysqli_error($conn);
+            }
+
+	}
+
+	# code...
+
+
+
+ 
+  
+   		
+      
+      $sql = "SELECT * FROM `users` WHERE `email` = '$userinfo'";
+     
+   //  echo $sql;
+
+  $result = mysqli_query($conn,$sql);// or die(mysql_error());
+ 
+      
+
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      
+       //  $active = $row['active'];
+
+		if ($result=mysqli_query($conn,$sql))
+  			{
+  // Fetch one and one row
+	  			while ($row=mysqli_fetch_row($result))
+	    		{
+		
+				?>
+
+
+
+	
+<div class="container-fluid text-center col-lg-5 align-items-center padding">
+	<div class="col-12"></div>
+	 <h1 class="display-4">Edit user Info</h1>
+	 <h3>Please fill in all the details</h3>
+	 <form method="post"  style="padding-bottom: 10px;">
+	 <div class="row">
+	 	<div class="col">
+	 		<input type="text" name="fname" class="form-control" placeholder="Firstname" required="required" value="<?php echo $row[1]; ?>">
+	 	</div>
+	 	<div class="col">
+	 		<input type="text" name="lname" class="form-control" placeholder="Lastname" required="required" value="<?php echo $row[2]; ?>">
+	 	</div>
+	 </div><br>
+	 <input type="email" name="email" class="form-control" placeholder="Email" required="required" value="<?php echo $row[4]; ?>"><br>
+	 
+	 <input type="date" name="dob" class="form-control" placeholder="Enter Your DOB" required="required" value="<?php echo $row[3]; ?>"><br>
+	 <div class="row">
+	 	<div class="col-3">
+	 		<label class="form-control">Department</label>
+	 	</div>
+	 	<div class="col-9">
+
+      <select name="department" class="form-control" value="<?php echo $row[6]; ?>">
+        
+        <option>Engineering</option>
+        <option>Human Resources</option>
+        <option>Technology</option>
+        <option>Administration</option>
+        <option>Assistance</option>
+        <option>Security</option>
+      </select>
+    <br>	 	</div>
+	 </div>
+	 <input type="number" name="phone" class="form-control" placeholder="Enter Your Number" required="required" value="<?php echo $row[8]; ?>"><br>
+	 <input type="submit" name="update" value="Update Info" class="btn btn-primary btn-lg form-control">
+
+	 </form>
+</div>
+
+
+	    				<?php
+ 
+
+				}
+  
+				
+			}
+      
+      
+      else{
+        	    	}
+
+        	    
+ include"bottom.php";  ?>
--- elevated/allusers.php
+++ elevated/allusers.php
@@ -3,9 +3,38 @@
 $i=1;
 
 
-      
-      $sql = "SELECT * FROM `users` WHERE `access_type`='User' OR `access_type`='Elevated'";
+       $sql = "SELECT * FROM `users` WHERE `access_type`='User' OR `access_type`='Elevated'";	
+
+      if(isset($_POST['ASC']))
+      	{
+      		$sql = "SELECT * FROM `users` WHERE `access_type`='User' OR `access_type`='Elevated' ORDER BY `fname` ASC";
+
+      	}
+
+      	elseif (isset($_POST['DSC'])) {
+      		# code...
+      	
+      $sql = "SELECT * FROM `users` WHERE `access_type`='User' OR `access_type`='Elevated' ORDER BY `fname` DESC";
      
+     }
+     elseif(isset($_POST['ASCT']))
+      	{
+      		$sql = "SELECT * FROM `users` WHERE `access_type`='User' OR `access_type`='Elevated' ORDER BY `dob` ASC";
+
+      	}
+
+      	elseif (isset($_POST['DSCT'])) {
+      		# code...
+      	
+      $sql = "SELECT * FROM `users` WHERE `access_type`='User' OR `access_type`='Elevated' ORDER BY `dob` DESC";
+     
+     }
+     else
+     {
+       $sql = "SELECT * FROM `users` WHERE `access_type`='User' OR `access_type`='Elevated'";	
+
+
+     }
      //echo $sql;
 
   $result = mysqli_query($conn,$sql);// or die(mysql_error());
@@ -24,12 +53,25 @@ $i=1;
   			<div class="container-fluid text-center col-lg-12 align-items-center padding">
   			<h1>All Users</h1>
   			<h6>Accept Admins</h6>
-  			<div class="container">
+  			<div class="container-fluid">
 			
-				<form method="POST">
+	<div class="row">
+	<div class="col-md-12 col-sm-12 col-xs-12 col-lg-3 text center">
+		<form method="POST" name="form2">
+			<input type="submit" name="ASC" class="btn btn-secondary list-item" value="Sort By Name(Ascending)">
+			<br><br>
+			<input type="submit" name="DSC" class="btn btn-secondary list-item" value="Sort By Name(Descending)">
+			<br><br>
+			<input type="submit" name="ASCT" class="btn btn-secondary list-item" value="Sort By Date of Birth (Ascending)">
+			<br><br>
+			<input type="submit" name="DSCT" class="btn btn-secondary list-item" value="Sort By Date of Birth (Descending)">
+			
 	
-			<div class="padding">
-				<table class="table table-dark">
+	 </div>				
+
+	
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-9">
+				<table class="table table-dark table-stripped table-hover">
   
 					  <thead>
 					    <tr>
@@ -38,8 +80,7 @@ $i=1;
 					      <th scope="col">Lastname</th>
 					      <th scope="col">Email</th>
 					      <th scope="col">DOB</th>
-
-					      <th scope="col">Department</th>
+						  <th scope="col">Department</th>
 					      <th scope="col">Access Type</th>
 					      <th scope="col">Phone</th>
 					      
@@ -68,7 +109,7 @@ $i=1;
 						      <td><?php echo $row[6];  ?></td>
 						      <td><?php echo $row[5];  ?></td>
 						      <td><?php echo $row[8]   ?></td>
-						      <td></td>
+						     
 						    </tr>
 						   
 	    			<?php
@@ -85,6 +126,7 @@ $i=1;
 				
       
       else{  echo "something went wrong";
+    
         	    	}
 
         	    
