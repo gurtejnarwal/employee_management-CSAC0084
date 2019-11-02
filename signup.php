67d170e47cb57d3d8662185f65fbabeb114620c6
c0734815 <44182257+c0734815@users.noreply.github.com>
2019-11-02 16:00:37 -0400

    Signup Captcha & Search USer

--- signup.php
+++ signup.php
@@ -2,38 +2,57 @@
 
 
 
-if (isset($_POST['Signup'])) {
+
+if(count($_POST)>0) {
+
+    if($_POST["captcha_code"]==$_SESSION["captcha_code"])
+    {
+
+        $success_message = "Your message received successfully";
+
+    	
+				if (isset($_POST['Signup'])) {
 
 
-	$fname=$_POST['fname'];
-	$lname=$_POST['lname'];
-	$email=$_POST['email'];
-	$pass1=$_POST['pass1'];
-	$pass2=$_POST['pass2'];
-	$dob=$_POST['dob'];
-	$phone=$_POST['phone'];
-	$department=$_POST['department'];
+				$fname=$_POST['fname'];
+				$lname=$_POST['lname'];
+				$email=$_POST['email'];
+				$pass1=$_POST['pass1'];
+				$pass2=$_POST['pass2'];
+				$dob=$_POST['dob'];
+				$phone=$_POST['phone'];
+				$department=$_POST['department'];
 
-	$at="User";
+				$at="User";
 
-	if ($pass1==$pass2) {
+				if ($pass1==$pass2) {
 
 
-		$sql="insert into `users` (`fname`,`lastname`,`dob`,`email`,`access_type`,`department`,`pass`,`phone`) values ('$fname','$lname','$dob','$email','$at','$department','$pass1','$phone')";
+					$sql="insert into `users` (`fname`,`lastname`,`dob`,`email`,`access_type`,`department`,`pass`,`phone`) values ('$fname','$lname','$dob','$email','$at','$department','$pass1','$phone')";
 
-		//echo $sql;
+					//echo $sql;
 
-		 if (mysqli_query($conn, $sql)) {
-               echo "New record created successfully";
-            } 
-            else {
-               echo "Error: " . $sql . "" . mysqli_error($conn);
-            }
+					 if (mysqli_query($conn, $sql)) {
+			               echo "New record created successfully";
+			            } 
+			            else {
+			               echo "Error: " . $sql . "" . mysqli_error($conn);
+			            }
 
+				}
+
+				# code...
+			}
 	}
 
-	# code...
+
+    else{
+        $error_message = "Incorrect Captcha Code";
+    }
+
 }
+
+
 
 ?>
 <div class="container-fluid text-center col-lg-5 align-items-center padding">
@@ -67,13 +86,43 @@ if (isset($_POST['Signup'])) {
         <option>Technology</option>
         <option>Administration</option>
         <option>Assistance</option>
-        <option>Security</option>
+        <option>Security</option>	
       </select>
     <br>	 	</div>
 	 </div>
-	 <input type="number" name="phone" class="form-control" placeholder="Enter Your Number" required="required"><br>
-	 <input type="submit" name="Signup" value="Sign Up" class="btn btn-primary btn-lg form-control">
+	 <input type="number" name="phone" class="form-control" placeholder="Enter Your Number" required="required">
+	 
+	 <br>
+	 <div class="row">
+	 
+	 	<div class="col-4">
+	 		<label class="form-control">Enter Captcha Code</label>
+	 	</div>
+	 
+	 	<div class="col-8" class="form-control">
+	 		
+	 		<div id="error-captcha">
+	 			<?php if(isset($error_message)) 
+	 			{ echo $error_message; } ?>
+	 				
+	 		</div>
+			
+	 		<input name="captcha_code" type="text" class="captcha-input form-control">
+	 		
+	 		</div>
 
+	 		</div>
+	 	
+	 <br>
+	 <input type="submit" name="Signup" value="Sign Up" class="btn btn-primary btn-lg form-control">
+<div class="row">
+	<div class="col-12">
+		
+<?php if(isset($success_message)) { ?>
+<div class="demo-success"><?php echo $success_message; ?></div>
+<?php } ?>
+	</div>
+</div>
 	 </form>
 </div>
 
